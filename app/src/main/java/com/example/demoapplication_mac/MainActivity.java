88ad01a33d755c6b8bc2361dc344bf4fa7047b9b
Tim Vernaeve <timvernaeve@Tims-MacBook-Pro.local>
2022-02-15 17:33:39 +0100

    initial commit on mac

---
+++ app/src/main/java/com/example/demoapplication_mac/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.demoapplication_mac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mijnKnop = findViewById(R.id.mijnbutton);
+        mijnKnop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView mijntextview = findViewById(R.id.mijntextview);
+                mijntextview.setText("Hallo Tim");
+            }
+        });
+
+    }
+}
